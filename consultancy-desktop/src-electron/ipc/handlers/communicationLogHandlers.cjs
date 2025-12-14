c9281c35bd46da967079e876235039e3409e1701
shiva00s <prakashshiva368@gmail.com>
2025-12-14 21:52:08 +0530

    Auto commit from push_all

--- consultancy-desktop/src-electron/ipc/handlers/communicationLogHandlers.cjs
+++
@@ -1,7 +0,0 @@
-const registerCommunicationLogHandlers = (ipcMain, dependencies) => {
-    const { queries } = dependencies;
-
-    ipcMain.handle('log-communication', (event, args) => queries.logCommunication(args.user, args.candidateId, args.type, args.details));
-};
-
-module.exports = { registerCommunicationLogHandlers };
